1bd1ee7179dfffab6bfeb6885f0a4087db72f3e2
André Wolski <locke@dena-design.de>
2019-05-22 13:28:29 +0200

    example4: remember parsed base

--- src/test/java/at/ac/tuwien/shacl/plugin/util/TestUtil4.java
+++ src/test/java/at/ac/tuwien/shacl/plugin/util/TestUtil4.java
@@ -1,10 +1,15 @@
 package at.ac.tuwien.shacl.plugin.util;
 
 import org.apache.jena.ontology.OntDocumentManager;
-import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.ontology.impl.OntModelImpl;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFLanguages;
+import org.apache.jena.riot.RDFParser;
+import org.apache.jena.riot.system.StreamRDF;
+import org.apache.jena.riot.system.StreamRDFLib;
+import org.apache.jena.riot.system.StreamRDFWrapper;
 import org.apache.jena.util.FileUtils;
 
 import java.io.IOException;
@@ -18,10 +23,6 @@ import at.ac.tuwien.shacl.plugin.events.ShaclValidation;
  */
 public class TestUtil4 {
 
-    private static final String example4aIRI = "http://www.example.org/names";
-    private static final String example4bIRI = "http://www.example.org/persons";
-    private static final String example4DataIRI = "http://www.example.org/";
-
     private static final URL example4aUrl = ShaclValidation.class.getClassLoader().getResource("example4a.ttl");
     private static final URL example4bUrl = ShaclValidation.class.getClassLoader().getResource("example4b.ttl");
     private static final URL example4DataUrl = ShaclValidation.class.getClassLoader().getResource("example4-data.owl.ttl");
@@ -30,7 +31,7 @@ public class TestUtil4 {
     public static Model getDataModel() throws IOException {
         try (InputStream in = example4DataUrl.openStream()) {
             Model dataModel = ModelFactory.createDefaultModel();
-            dataModel.read(in, example4DataIRI, FileUtils.langTurtle);
+            dataModel.read(in, null, FileUtils.langTurtle);
 
             return dataModel;
         }
@@ -43,20 +44,66 @@ public class TestUtil4 {
 		spec.setDocumentManager(dm);
 
         try {
-            OntModel model4a = loadOntModel(spec, example4aUrl, example4aIRI);
-            dm.addModel(example4aIRI, model4a);
-            return loadOntModel(spec, example4bUrl, example4bIRI);
+            OntModelWithIRI model4a = loadOntModel(spec, example4aUrl);
+            dm.addModel(model4a.getBaseIRI(), model4a);
+            return loadOntModel(spec, example4bUrl);
         } catch (IOException e) {
             e.printStackTrace();
             throw e;
         }
     }
 
-    private static OntModel loadOntModel(OntModelSpec spec, URL url, String base) throws IOException {
-        OntModel model = ModelFactory.createOntologyModel(spec);
+    private static OntModelWithIRI loadOntModel(OntModelSpec spec, URL url) throws IOException {
+        OntModelWithIRI model = new OntModelWithIRI(spec);
         try (InputStream in = url.openStream()) {
-            model.read(in, base, FileUtils.langTurtle);
+            StreamRDFWrapperWithIRI sink = new StreamRDFWrapperWithIRI(StreamRDFLib.graph(model.getGraph()));
+
+            RDFParser.source(in).lang(RDFLanguages.nameToLang(FileUtils.langTurtle)).parse(sink);
+
+            model.setBaseIRI(sink.getBaseIRI());
+
+            model.loadImports();
+            model.rebind();
+
             return model;
+        }
+    }
+
+    private static class OntModelWithIRI extends OntModelImpl {
+        private String baseIRI = null;
+
+        public OntModelWithIRI(OntModelSpec spec, Model model) {
+            super(spec, model);
+        }
+
+        public OntModelWithIRI(OntModelSpec spec) {
+            super(spec);
+        }
+
+        public void setBaseIRI(String baseIRI) {
+            this.baseIRI = baseIRI;
+        }
+
+        public String getBaseIRI() {
+            return baseIRI;
+        }
+    }
+
+    private static class StreamRDFWrapperWithIRI extends StreamRDFWrapper {
+        private String baseIRI = null;
+
+        public StreamRDFWrapperWithIRI(StreamRDF other) {
+            super(other);
+        }
+
+        @Override
+        public void base(String base) {
+            other.base(base);
+            this.baseIRI = base;
+        }
+
+        public String getBaseIRI() {
+            return baseIRI;
         }
     }
 
